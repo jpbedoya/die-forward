6112d7834e989c92972944e4e58bd2f8e52e8b83
jpbedoya <JP.bedoya@yahoo.com>
2026-02-07 22:40:46 -0800

    Add redirect: /slides -> /slides/index.html

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/slides',
+        destination: '/slides/index.html',
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
